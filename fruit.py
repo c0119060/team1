87d656ca8b581c6b12456f820daabb86173976c6
TouiAndou <74704982+TouiAndou@users.noreply.github.com>
2021-12-09 11:42:07 +0900

    サンプル

---
+++ fruit.py
@@ -0,0 +1,7 @@
+mylist = ["Orange", "Peach", "Lemon", "Apple"]
+for val in mylist:
+    print("value:" + val)
+    
+mydict = {"L":"Lemon", "O":"Orage", "G":"Grapes"}
+for mykey, myvalue in mydict.items():
+    print("key:" + mykey + ", value:" + myvalue)
